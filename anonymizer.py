12e42c250f433e28c2cfc1723b6fd1dbae2af67f
abderder <abderrazak.safi@gmail.com>
2024-10-21 15:55:49 +0200

    ajout csv file upload

--- anonymizer.py
+++ anonymizer.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Anonymisation des données CSV")
+data_file = st.file_uploader("Télécharger le fichier CSV avec les informations personnelles : ", type=["csv"])
+correspondance_file = st.file_uploader("Télécharger le fichier CSV de correspondance : ", type=["csv"])
 
-st.write("hello world!")
+if data_file is not None and correspondance_file is not None:
+    data_df = pd.read_csv(data_file)
+    correspondance_df = pd.read_csv(correspondance_file)
+
+
